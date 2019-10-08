d804baa24f10c16b2b88a42f542691462b1a8ac1
bubxtech <40369251+bubxtech@users.noreply.github.com>
2019-10-08 12:52:37 +0200

    Add files via upload
    init commit

---
+++ NeuralNetServer/src/main/java/pl/jwalczak/NeuralNetServer/NeuralNetServerApplication.java
@@ -0,0 +1,13 @@
+package pl.jwalczak.NeuralNetServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralNetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralNetServerApplication.class, args);
+	}
+
+}
